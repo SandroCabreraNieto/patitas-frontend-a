6bcca7a9a8176477f375d2e3855b72b60e1165f5
Sandro Cabrera <cabrerasandro682@gmail.com>
2024-09-22 11:56:50 -0500

    Consumo patitas-backend-a

--- src/main/java/pe/edu/cibertec/adopta_patitas/Controler/LoginController.java
+++ src/main/java/pe/edu/cibertec/adopta_patitas/Controler/LoginController.java
@@ -1,16 +1,23 @@
 package pe.edu.cibertec.adopta_patitas.Controler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import pe.edu.cibertec.adopta_patitas.dto.LoginRequestDTO;
+import pe.edu.cibertec.adopta_patitas.dto.LoginResponseDTO;
 import pe.edu.cibertec.adopta_patitas.viewmodel.LoginModel;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/inicio")
     public String inicio(Model model) {
@@ -26,9 +33,9 @@ public class LoginController {
                              Model model) {
 
         //Validar campos de entrada
-        if(tipoDocumento == null || tipoDocumento.trim().length() == 0 ||
-            numeroDocumento == null || numeroDocumento.trim().length() == 0 ||
-            password == null || password.trim().length() == 0) {
+        if (tipoDocumento == null || tipoDocumento.trim().length() == 0 ||
+                numeroDocumento == null || numeroDocumento.trim().length() == 0 ||
+                password == null || password.trim().length() == 0) {
 
             LoginModel loginModel = new LoginModel("01", "Error: Debe completar correctamente sus credenciale", "");
             model.addAttribute("loginModel", loginModel);
@@ -36,10 +43,20 @@ public class LoginController {
         }
 
         //Invocar servicio de autenticacion
+            //Objeto para la solicitud
+            LoginRequestDTO loginRequest = new LoginRequestDTO(tipoDocumento, numeroDocumento, password);
 
+            //Consumir API
+            LoginResponseDTO response = restTemplate.postForObject("http://localhost:8081/autenticacion/login", loginRequest, LoginResponseDTO.class);
 
-        LoginModel loginModel = new LoginModel("00", "", "Cesar Santos");
-        model.addAttribute("loginModel", loginModel);
-        return "principal";
+            if (response != null && "00".equals(response.codigo())) {
+                LoginModel loginModel = new LoginModel("00", "", response.nombreUsuario());
+                model.addAttribute("loginModel", loginModel);
+                return "principal";
+            } else {
+                LoginModel loginModel = new LoginModel("01", response.mensaje(), "");
+                model.addAttribute("loginModel", loginModel);
+                return "inicio";
+            }
     }
 }
---
+++ src/main/java/pe/edu/cibertec/adopta_patitas/dto/LoginResponseDTO.java
@@ -0,0 +1,5 @@
+package pe.edu.cibertec.adopta_patitas.dto;
+
+public record LoginResponseDTO(String codigo, String mensaje, String nombreUsuario, String correoUsuario) {
+
+}
